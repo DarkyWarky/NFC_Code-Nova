a97ee521fdec8b5f9891436651fbec037b573bef
DarkyWarky <121173192+DarkyWarky@users.noreply.github.com>
2023-09-16 05:13:19 +0530

    done filter

--- routes/routers.js
+++ routes/routers.js
@@ -9,7 +9,11 @@ const fetchUniversitiesByLocation4 =require ('../Database/canada')
 const fetchUniversitiesByLocation5 =require ('../Database/pakistan')
 const fetchUniversitiesByLocation6 =require ('../Database/united-kingdom')
 const fetchUniversitiesByLocation7 =require ('../Database/south-africa')
-const fetchUniversitiesByLocation8 =require ('../Database/mongos')
+const fetchUniversitiesByDegree =require ('../Database/degree/master')
+const fetchUniversitiesByDegree1 =require ('../Database/degree/phd')
+const fetchUniversitiesByDegree2 =require ('../Database/degree/bachelor')
+const fetchUniversitiesByDegree3 =require ('../Database/degree/course')
+
 
 router.get("/", async (req, res) => {
   try {
@@ -190,6 +194,96 @@ router.get("/united-kingdom", async (req, res) => {
 router.get("/south-africa", async (req, res) => {
   try {
     const universities = await fetchUniversitiesByLocation7();
+
+    if (!universities || universities.length === 0) {
+      // Handle the case where no universities are found
+      res.render("Resourcehub", { error: "No universities found." });
+      return;
+    }
+
+    let data = {
+      css: '../public/css/Resourcehub.css',
+      script: '../public/js/Resourcehub.js',
+      universities: universities
+    };
+
+    res.render("Resourcehub", data);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Degrees Filter
+router.get("/master", async (req, res) => {
+  try {
+    const universities = await fetchUniversitiesByDegree();
+
+    if (!universities || universities.length === 0) {
+      // Handle the case where no universities are found
+      res.render("Resourcehub", { error: "No universities found." });
+      return;
+    }
+
+    let data = {
+      css: '../public/css/Resourcehub.css',
+      script: '../public/js/Resourcehub.js',
+      universities: universities
+    };
+
+    res.render("Resourcehub", data);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+router.get("/phd", async (req, res) => {
+  try {
+    const universities = await fetchUniversitiesByDegree1();
+
+    if (!universities || universities.length === 0) {
+      // Handle the case where no universities are found
+      res.render("Resourcehub", { error: "No universities found." });
+      return;
+    }
+
+    let data = {
+      css: '../public/css/Resourcehub.css',
+      script: '../public/js/Resourcehub.js',
+      universities: universities
+    };
+
+    res.render("Resourcehub", data);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+router.get("/bachelor", async (req, res) => {
+  try {
+    const universities = await fetchUniversitiesByDegree2();
+
+    if (!universities || universities.length === 0) {
+      // Handle the case where no universities are found
+      res.render("Resourcehub", { error: "No universities found." });
+      return;
+    }
+
+    let data = {
+      css: '../public/css/Resourcehub.css',
+      script: '../public/js/Resourcehub.js',
+      universities: universities
+    };
+
+    res.render("Resourcehub", data);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+router.get("/course", async (req, res) => {
+  try {
+    const universities = await fetchUniversitiesByDegree3();
 
     if (!universities || universities.length === 0) {
       // Handle the case where no universities are found
