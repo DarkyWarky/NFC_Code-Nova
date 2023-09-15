5fe68d2e0b07db2dc6b86700785c3137cee03780
DarkyWarky <121173192+DarkyWarky@users.noreply.github.com>
2023-09-15 12:18:29 +0530

    created folder and initial setup

---
+++ models/usermodel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const user = mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    collegeid:{
+        type:Number,
+        required:true
+    },
+    collegename:{
+        type:String,
+        required:true
+    },isverified:{
+        type:Boolean,
+        default:false
+    }
+})
+
+module.exports = mongoose.model("User",user)
